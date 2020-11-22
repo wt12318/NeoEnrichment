f770c1571e11e2b24c943789fd0d182ae0b31d81
wutao <1650464505@qq.com>
2020-11-22 10:00:48 +0800

    update get_cancer_type to add full name support

--- R/get_cancer_type.R
+++ R/get_cancer_type.R
@@ -4,58 +4,80 @@
 #' of sample barcode to extract cancer type based on the table of Tissue Source Site Codes of GDC, the description
 #' of this data can be seen at document by `?cancer_type_code`
 #'
-#' @param tumor_barcode TCGA tumor sample barcode
+#' @param input TCGA tumor sample barcode or TCGA cancer type full name
 #' @param parallel Bool value, use parallelization or not, default FALSE
 #' @param cores How many cores to use if parallelization is used, only can ce used when parallel is TRUE
 #' @param need_full_name Bool value, return TCGA cancer type full name or not, default FALSE
+#' @param input_type "code" or "full_name" for input of TCGA tumor sample barcode or TCGA cancer type full name, respectively.
 #'
 #' @return cancer type
 #' @export
-#' @examples get_cancer_type("TCGA-OR-A5J1",par=FALSE)
+#' @examples
+#' get_cancer_type("TCGA-OR-A5J1",par=FALSE)
+#' get_cancer_type("Liver hepatocellular carcinoma",input_type="full_name")
 
-get_cancer_type <- function(tumor_barcode,cores,parallel=FALSE,need_full_name=FALSE){
+get_cancer_type <- function(input,cores,parallel=FALSE,need_full_name=FALSE,input_type="code"){
 
   cancer_type_code <- NeoEnrichment::cancer_type_code
-  if (!need_full_name){
+  if (input_type=="code"){
+    if (!need_full_name){
+      if(!parallel){
+        code <- substr(input,6,7)
+        cancer_type <- sapply(code,
+                              function(x){
+                                cancer_type_code$cancer_type[grep(x,cancer_type_code$cancer_code)]
+                              })
+        return(cancer_type)
+      }else{
+        cat(paste0("you have ",detectCores(logical = F)," cores"))
+        cl <- makeCluster(getOption("cl.cores", cores),type="FORK")
+        code <- substr(input,6,7)
+        cancer_type <- parSapply(cl=cl,code,
+                                 function(x){
+                                   cancer_type_code$cancer_type[grep(x,cancer_type_code$cancer_code)]
+                                 })
+        stopCluster(cl)
+        return(cancer_type)
+      }
+    }else{
+      if(!parallel){
+        code <- substr(input,6,7)
+        cancer_type <- sapply(code,
+                              function(x){
+                                cancer_type_code$cancer_type_full_name[grep(x,cancer_type_code$cancer_code)]
+                              })
+        return(cancer_type)
+      }else{
+        cat(paste0("you have ",detectCores(logical = F)," cores"))
+        cl <- makeCluster(getOption("cl.cores", cores),type="FORK")
+        code <- substr(input,6,7)
+        cancer_type <- parSapply(cl=cl,code,
+                                 function(x){
+                                   cancer_type_code$cancer_type_full_name[grep(x,cancer_type_code$cancer_code)]
+                                 })
+        stopCluster(cl)
+        return(cancer_type)
+      }
+    }
+  }else if (input_type=="full_name"){
     if(!parallel){
-      code <- substr(tumor_barcode,6,7)
-      cancer_type <- sapply(code,
+      cancer_type <- sapply(input,
                             function(x){
-                              cancer_type_code$cancer_type[grep(x,cancer_type_code$cancer_code)]
+                              unique(cancer_type_code$cancer_type[grep(x,cancer_type_code$cancer_type_full_name)])
                             })
       return(cancer_type)
     }else{
       cat(paste0("you have ",detectCores(logical = F)," cores"))
       cl <- makeCluster(getOption("cl.cores", cores),type="FORK")
-      code <- substr(tumor_barcode,6,7)
-      cancer_type <- parSapply(cl=cl,code,
+      cancer_type <- parSapply(cl=cl,input,
                                function(x){
-                                 cancer_type_code$cancer_type[grep(x,cancer_type_code$cancer_code)]
+                                 cancer_type_code$cancer_type[grep(x,cancer_type_code$cancer_type_full_name)]
                                })
       stopCluster(cl)
       return(cancer_type)
     }
   }else{
-    if(!parallel){
-      code <- substr(tumor_barcode,6,7)
-      cancer_type <- sapply(code,
-                            function(x){
-                              cancer_type_code$cancer_type_full_name[grep(x,cancer_type_code$cancer_code)]
-                            })
-      return(cancer_type)
-    }else{
-      cat(paste0("you have ",detectCores(logical = F)," cores"))
-      cl <- makeCluster(getOption("cl.cores", cores),type="FORK")
-      code <- substr(tumor_barcode,6,7)
-      cancer_type <- parSapply(cl=cl,code,
-                               function(x){
-                                 cancer_type_code$cancer_type_full_name[grep(x,cancer_type_code$cancer_code)]
-                               })
-      stopCluster(cl)
-      return(cancer_type)
-    }
+    cat("input_type must be 'code' or 'full_name' ")
   }
-
-
 }
 
