caf2a50b3a1746063da1d6cd9ffb9273227bca8c
wutao <1650464505@qq.com>
2021-06-17 02:46:38 -0400

    compare two distributions

--- R/cal_es_new_test.R
+++ R/cal_es_new_test.R
@@ -7,40 +7,46 @@
 #' @export
 #'
 #' @examples
-cal_es_new_test <- function(dt,type){
+cal_es_new_test <- function(dt){
 
-  n_neo <- sum(dt$neo=="yes")
-  n_sys <- sum(dt$neo=="no")
-  interval <- seq(0,1,length.out=max(n_neo,n_sys)) %>%
-    round(.,digits = 2)
 
-  if (type == "neo"){
-    obj <- dt %>%
-      dplyr::filter(neo == "yes")
-  }else{
-    obj <- dt %>%
-      dplyr::filter(neo == "no")
-  }
+  interval <- seq(0,1,length.out=100)
+  obj_neo <- dt %>%
+    dplyr::filter(neo == "yes")
+  obj_sys <- dt %>%
+    dplyr::filter(neo == "no")
 
-  tmp_obj <- table(cut(obj$ccf,interval)) %>% as.data.frame()
-
-  if (nrow(obj)==0){
+  tmp_obj_neo <- table(cut(obj_neo$ccf,interval)) %>% as.data.frame()
+  tmp_obj_sys <- table(cut(obj_sys$ccf,interval)) %>% as.data.frame()
+  if (nrow(tmp_obj_neo)==0){
     return("there are no neoantigen/no_neoantigen in this sample")
   }else{
-    ll <- nrow(tmp_obj)
-    tmp_obj$Freq[1] <- tmp_obj$Freq[1] + sum(obj$ccf==0)
-    tmp_obj <- tmp_obj %>%
-      mutate(index = c(1:nrow(tmp_obj))) %>%
+    ll <- nrow(tmp_obj_neo)
+    tmp_obj_neo$Freq[1] <- tmp_obj_neo$Freq[1] + sum(obj_neo$ccf==0)
+    tmp_obj_neo <- tmp_obj_neo %>%
+      mutate(index = c(1:nrow(tmp_obj_neo))) %>%
       arrange(desc(index))%>%
       mutate(rank=abs((ll/2)-index)+1)
 
-    add <- 1/sum(tmp_obj[tmp_obj$Freq!=0,"rank"]*tmp_obj[tmp_obj$Freq!=0,"Freq"])
-    minus <- 1/sum(tmp_obj$Freq==0)
+    tmp_obj_sys$Freq[1] <- tmp_obj_sys$Freq[1] + sum(obj_sys$ccf==0)
+    tmp_obj_sys <- tmp_obj_sys %>%
+      mutate(index = c(1:nrow(tmp_obj_sys))) %>%
+      arrange(desc(index))
 
-    tmp_obj$re <- ifelse(tmp_obj$Freq == 0,-minus,
-                         ((tmp_obj$rank)*tmp_obj$Freq)*add)
-    tmp_obj$cum_re <- cumsum(tmp_obj$re)
-    es <- max(0,tmp_obj$cum_re)-abs(min(tmp_obj$cum_re,0))
+    add <- 1/sum(tmp_obj_neo[tmp_obj_neo$Freq!=0,"rank"]*tmp_obj_neo[tmp_obj_neo$Freq!=0,"Freq"])
+    add2 <- 1/sum(tmp_obj_sys$Freq!=0)
+
+    tmp_obj_neo$re <- ifelse(tmp_obj_neo$Freq == 0,0,
+                         ((tmp_obj_neo$rank)*tmp_obj_neo$Freq)*add)
+    tmp_obj_neo$cum_re <- cumsum(tmp_obj_neo$re)
+
+    tmp_obj_sys$re <- ifelse(tmp_obj_sys$Freq == 0,0,
+                             add2)
+    tmp_obj_sys$cum_re <- cumsum(tmp_obj_sys$re)
+
+    diff <- tmp_obj_neo$cum_re - tmp_obj_sys$cum_re
+
+    es <- max(0,diff)-abs(min(diff,0))
     return(es)
   }
 }
